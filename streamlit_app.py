4d7b33a74958845767220a2a54d7faeeb668c68d
leepillen <35201411+leepillen@users.noreply.github.com>
2023-02-07 11:52:17 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
-import streamlit as sl
+import streamlit
 
-sl.title('My Parents New Healthy Diner')
+streamlit.title('My Parents New Healthy Diner')
 
-sl.header('Breakfast Menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
